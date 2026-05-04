484f47002bb92cfcc7d84c0bd51ebe86e263095e
Gustavo Leal <gustavovallim75@gmail.com>
2026-05-03 23:25:12 -0300

    Finalizado o modo tema e construindo o componente pages

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,11 +1,20 @@
-import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from "lucide-react";
+import { HistoryIcon, HouseIcon, MoonIcon, SettingsIcon, SunIcon } from "lucide-react";
 import styles from "./styles.module.css";
 import { useState, useEffect } from "react";
 
 type AvailableThemes = "dark" | "light";
 
 export function Menu() {
-  const [theme, setTheme] = useState<AvailableThemes>("dark");
+  const [theme, setTheme] = useState<AvailableThemes>(() => {
+    const StorageTheme =
+      (localStorage.getItem("theme") as AvailableThemes) || "dark";
+    return StorageTheme;
+  });
+
+  const nextThemeIcon = {
+    dark: <SunIcon />,
+    light: <MoonIcon />,
+  };
 
   function handleThemeChange(
     event: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
@@ -20,13 +29,12 @@ export function Menu() {
   }
 
   useEffect(() => {
-    console.log("Theme mudou", theme);
     document.documentElement.setAttribute("data-theme", theme);
+    localStorage.setItem("theme", theme);
   }, [theme]);
 
   return (
     <nav className={styles.menu}>
-      <h1>{theme}</h1>
       <a className={styles.menuLink} href="#" aria-label="Menu" title="Menu">
         <HouseIcon />
       </a>
@@ -53,7 +61,7 @@ export function Menu() {
         title="Tema"
         onClick={handleThemeChange}
       >
-        <SunIcon />
+        {nextThemeIcon[theme]}
       </a>
     </nav>
   );
